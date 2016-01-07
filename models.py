e4d9c395abf6bf8043ebc1bd57e549d3b906851e
Ahmed Carto <ahmednoureldeen@cartologic.com>
2016-01-07 12:20:06 +0200

    new/edit form enchancements

--- models.py
+++ models.py
@@ -7,6 +7,5 @@ from cartoview.app_manager.models import AppInstance
 # Create your models here.
 
 class LocalPerspective(AppInstance):
-    # note the related_name format to avoid conflicts between derived models
     web_map_id = UUIDField()
     config = models.TextField(null=True, blank=True)
